79f9178a9f4aec80ab999eb908c5d1e105b51c52
Rix <rihard.konnapere@tktk.ee>
2023-06-01 12:48:03 +0300

    Add week 5 code

---
+++ ANDOp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int math, bio, chem;
+
+Console.WriteLine("Enter your maths result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your bio results:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chem results:");
+chem = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && bio >= 90 && chem >= 90)
+{
+    Console.WriteLine("Congratulatons, you have been accepted!");
+}
+else
+{
+    Console.WriteLine("Sorry, your scores are too low.");
+}
